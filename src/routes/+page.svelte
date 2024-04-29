a6243443eb18f92191a6041d8739e1434bb9d99e
break-the-build <145877102+break-the-build@users.noreply.github.com>
2024-04-29 14:49:03 -0700

    feat: add introduction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,7 @@
-Daniel Luedke
-
-<div class="mx-4">
-<ol class="relative border-s border-gray-200 dark:border-gray-700">                  
-    <li class="mb-10 ms-4">
-        <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-        <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2022-2023</time>
-        <h3 class="text-lg font-semibold text-gray-900 dark:text-gray-800"><a href='https://synopsys.com'>Synopsys</a></h3>
-        <h4 class="text-sm font-semibold text-gray-500 dark:text-gray-800">Principal Sales Engineer, Software Integrity Group</h4>
-        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
-        <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Learn more <svg class="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-  </svg></a>
-    </li>
-    <li class="mb-10 ms-4">
-        <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-        <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2021</time>
-        <h3 class="text-lg font-semibold text-gray-900 dark:text-gray-800"><a href='https://nobl9.com'>Nobl9</a></h3>
-        <p class="text-base font-normal text-gray-500 dark:text-gray-400">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
-    </li>
-    <li class="mb-10 ms-4">
-        <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-        <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2019</time>
-        <h3 class="text-lg font-semibold text-gray-900 dark:text-gray-800">Dragonchain</h3>
-        <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements built on top of Tailwind CSS.</p>
-    </li>
-    <li class="mb-10 ms-4">
-        <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-        <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2018</time>
-        <h3 class="text-lg font-semibold text-gray-900 dark:text-gray-800">F5</h3>
-        <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements built on top of Tailwind CSS.</p>
-    </li>
-</ol>
+<div class="bg-white shadow-md rounded-lg p-6 max-w-lg mx-auto mt-10">
+	<h1 class="text-2xl font-semibold text-gray-900 mb-2">Welcome! I'm Daniel Luedke,</h1>
+	<p class="text-gray-700">
+		a technologist with deep expertise in Sales Engineering and Product Marketing. I specialize in
+		scalable web applications, developer security, and cloud engineering.
+	</p>
 </div>
